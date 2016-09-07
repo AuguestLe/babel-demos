a3f578a4bcfc82c2e567f9e2eeb48966656207d0
wanglebus <502732634@qq.com>
2016-09-07 19:13:27 +0800

    fixe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 console.log('===');
-const sum=(a,b)=>a+b;
+const sum = (a,b) => a + b;
 let x = sum(1,2);
 let y = sum(1,2);
 console.log(x);
+
+
+
+
+
+// 上传
+// inndaejson{
+// 	contractPicIds->arr-> 合同上传						====>contractPicIds
+//
+// 	businessLicencePicIds->arr1-> 营业执照副本			====>businessLicencePicIds
+//
+// 	spIndustryLicensePicIds->arr2-> 特种行业许可证		====>spIndustryLicensePicIds
+//
+// 	businessEntityIdcardPicIds->arr3-> 企业法人身份证 	====>businessEntityIdcardPicIds
+//
+// 	competitorPricePicIds->arr4-> 竞争价格截图			====>competitorPricePicIds
+// }
+//
+// ---------------------------------------------------------------------
+// 下载
+// inndaejson{
+// 	contractPics->arr-> 合同上传						====>contractPicIds
+//
+// 	businessLicencePics->arr1-> 营业执照副本			====>businessLicencePicIds
+//
+// 	spIndustryLicensePics->arr2-> 特种行业许可证		====>spIndustryLicensePicIds
+//
+// 	businessEntityIdcardPics->arr3-> 企业法人身份证 	====>businessEntityIdcardPicIds
+//
+// 	competitorPricePics->arr4-> 竞争价格截图			====>competitorPricePicIds
+// }
